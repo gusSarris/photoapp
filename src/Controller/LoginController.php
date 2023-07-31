9a2f546e8bd6620eaee0a60958e56717148a6fa1
sarris kostas <sarriskonst@gmail.com>
2023-07-31 21:13:47 +0300

    adding links to  header

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,7 +6,6 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
